25e563a9c32d95e45d2fc6c5bff1e024b48683b0
Miguel Gondres <42485704+Einst3in@users.noreply.github.com>
2022-07-23 11:23:11 -0400

    fixes

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import { CounterApp } from "../src/CounterApp"
 
 describe('Pruebas <CounterApp/>', () => {
@@ -6,17 +6,17 @@ describe('Pruebas <CounterApp/>', () => {
     const initail_value = 100
 
     test('debe de hacer match con el snapshot', ()=>{
-        const {container} = render(<CounterApp value={initail_value}/>)
+        const {container} = render(<CounterApp value={100}/>)
         expect(container).toMatchSnapshot()
     })
 
     test('debe de mostrar el valor inicial de 100', ()=>{
         render(
             <CounterApp
-                value={initail_value}
+                value={100}
             />
         )
-        expect(initail_value).toBeTruthy()
+        expect(screen.getByText(0)).toBeTruthy()
     })
 
  })
