136a435b08d7ff1043f57bfb534792de297d6055
Miguel Gondres <42485704+Einst3in@users.noreply.github.com>
2022-07-23 11:12:14 -0400

    tarea CounterApp

---
+++ tests/CounterApp.test.jsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react"
+import { CounterApp } from "../src/CounterApp"
+
+describe('Pruebas <CounterApp/>', () => {
+    
+    const initail_value = 100
+
+    test('debe de hacer match con el snapshot', ()=>{
+        const {container} = render(<CounterApp/>)
+        expect(container).toMatchSnapshot()
+    })
+
+    test('debe de mostrar el valor inicial de 100', ()=>{
+        render(
+            <CounterApp
+                value={initail_value}
+            />
+        )
+        expect(initail_value).toBeTruthy()
+    })
+
+ })
